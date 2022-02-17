f24a5519599545274e0b1dea28be842e40b532ad
Simon Huwiler <webmaster@simonhuwiler.ch>
2022-02-17 15:41:54 +0100

    Update settings.py
    cleanup

--- src/settings.py
+++ src/settings.py
@@ -1,20 +1,7 @@
 from pathlib import Path
 
 users = {
-    'tt1ds': ['#doggy', '#dog'],
-    # 'ttalfred': ['#doggy', '#dog'],
-    'tt2ds': ['sadgirl', 'sad', 'pain', 'sadstory', 'depression', 'dog', 'puppy'],
-    'tt3ds2': ['summer', 'fashion'],
-    'tt4ds': ['sad', 'broken', 'dead', 'crying', 'anxiety', 'pain', 'depression', 'dog', 'puppy'],
-    'tt5ds': ['sad', 'broken', 'dead', 'crying', 'anxiety', 'pain', 'depression', 'dog', 'puppy'],
-    'tt6ds': ['summer', 'fashion'],
-    'tt7ds': ['allah', 'islam', 'mufti', 'meka', 'quran'],
-    #'tt8ds': ['zertifikat', 'covid', 'corona', 'pandemie', 'pandemic', 'virus', 'impfung', 'vaccin', 'lockdown'],
-    'tt8ds': ['mood', 'love', 'sick', 'pain', 'sad', 'anxiety', 'depression', 'couple', 'dark'],
-    'ttalfred': ['mood', 'love', 'sick', 'pain', 'sad', 'anxiety', 'depression', 'couple', 'dark'],
-    'is4daspe': ['mood', 'love', 'sick', 'pain', 'sad', 'anxiety', 'depression', 'couple', 'dark'],
-    'is6daspe': ['beauty', 'fashion', 'mylook', 'teen'],
-    'annabab_i1': ['beauty', 'fashion', 'mylook', 'teen', 'style'],
+    'myaccount': ['#doggy', '#dog']
 }
 
 # run_for_seconds = 1700
@@ -25,10 +12,6 @@ avds = {
     "port": 4723,
     "avd": "annabab_i1"
   },
-  "bot4": {
-    "port": 4723,
-    "avd": "smbot_4"
-  }
 }
 
 desired_cap = {
@@ -43,9 +26,7 @@ desired_cap = {
   "waitForIdleTimeout": 0,
 }
 
-# tiktok_app = '/Users/simon/Documents/projects/nzz_tiktok_insta_bot/lib/TikTok_v20.9.3_apkpure.com.apk'
 tiktok_app = 'com.zhiliaoapp.musically'
-# instagram_app = '/Users/simon/Documents/projects/nzz_tiktok_insta_bot/lib/Instagram_v202.0.0.37.123_apkpure.com.apk'
 instagram_app = 'com.instagram.android'
 
 data_root = Path('../../nzz_tiktok_insta_bot_data/export/')
